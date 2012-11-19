f87f599666aa303f3bccc9d72b2e3d77319d2057
Jhon.Liu <jhon.liu@splashtop.com>
2012-11-19 20:27:14 +0800

    FEATURE: Project init DESC: None

---
+++ src/com/rex/accmeter/GraphActivity.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rex.accmeter;
+
+import android.app.Activity;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import com.rex.accmeter.R;
+
+public class GraphActivity extends Activity {
+
+	static final private boolean DEBUG = true;
+	static final private String TAG = "RaxLog";
+	
+    private SensorManager mSensorManager;
+    private GraphView mGraphView;
+    
+	public GraphActivity() {
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (DEBUG) Log.v(TAG, "GraphActivity::onCreate");
+		
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mGraphView = new GraphView(this);
+        setContentView(mGraphView);
+	}
+	
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (DEBUG) Log.v(TAG, "GraphActivity::onResume");
+        mSensorManager.registerListener(mGraphView, 
+                SensorManager.SENSOR_ACCELEROMETER | 
+                SensorManager.SENSOR_MAGNETIC_FIELD | 
+                SensorManager.SENSOR_ORIENTATION,
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    
+    @Override
+    protected void onStop() {
+    	if (DEBUG) Log.v(TAG, "GraphActivity::onStop");
+        mSensorManager.unregisterListener(mGraphView);
+        super.onStop();
+    }
+}
---
+++ src/com/rex/accmeter/TabGraphActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rex.accmeter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import com.rex.accmeter.R;
+
+public class TabGraphActivity extends Activity implements OnClickListener {
+
+	static final private boolean DEBUG = true;
+	static final private String TAG = "RaxLog";
+	
+	public TabGraphActivity() {
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (DEBUG) Log.v(TAG, "GraphActivity::onCreate");
+		
+		setContentView(R.layout.graph_activity);
+		
+		((Button) findViewById(R.id.btn_start_graph)).setOnClickListener(this);
+		
+	}
+	
+	@Override
+	public void onBackPressed() {
+		getParent().onBackPressed();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_start_graph:
+			Intent intent = new Intent(this, GraphActivity.class);
+			startActivity(intent);
+			break;
+		}
+	}
+}
